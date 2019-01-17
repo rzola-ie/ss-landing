59297fae520d0838771a0061c5923b776729b1dd
ryanzola <ryanzola@me.com>
2019-01-17 15:04:02 -0500

    GSAP BoilerPlate
    - Implements gsap starter code in index.js
    - Implements canvas id in css and hides canvas

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('hello parcel');
+import { TweenMax, TimelineMax, Back, Elastic, Bounce, Power4, Linear, SteppedEase } from 'gsap/all';
+import jquery from "jquery";
+
+window.$ = window.jQuery = jquery;
+
+console.log('hello');
+
+// declare actors
+const $canvas = $('#canvas');
+
+// clear stage
+function clearStage() {
+  let clearTl = new TimelineMax();
+  clearTl
+    .set($canvas, {y: (window.outerHeight / 2) - 102.9, x: (window.innerWidth / 2) - 250, onComplete: showCanvas})
+
+  return clearTl
+}
+
+// show canvas on clearTl completion
+function showCanvas() {
+  $canvas.css('visibility', 'visible');
+}
+
+// master timeline
+let masterTl = new TimelineMax();
+masterTl
+  .add(clearStage(), 'clear-stage')
